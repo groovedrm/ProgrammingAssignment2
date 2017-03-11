8009e2fc9ef3a2f9abdec027561d922584d69784
ccocuzzo <ccocuzzo@nephilaadvisors.com>
2017-03-11 14:32:29 -0500

    Commit for course submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# First function to variable assignment, get, set, etc.
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv_mtx <- NULL
+  set <- function(y) {
+    x <<- y
+    inv_mtx <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv_mtx <<- inverse
+  getinverse <- function() inv_mtx
+  list(set=set, 
+       get=get, 
+       setinverse=setinverse, 
+       getinverse=getinverse
+  )
 }
 
-
-## Write a short comment describing this function
-
+# For inverting, looking in cache, retrieving from cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv_mtx <- x$getinverse()
+  if(!is.null(inv_mtx)) {
+    message("Loading from cache")
+    return(inv_mtx)
+  }
+  data <- x$get()
+  inv_mtx <- solve(data)
+  x$setinverse(inv_mtx)
+  inv_mtx
 }
